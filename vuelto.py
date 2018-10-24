e7e826d6051fb4bf1797ffd7958f7eebcda685c4
Jose <aldairxd11@hotmail.com>
2018-10-24 17:45:26 -0500

    Comenzando v1 creando contador de billetes

--- vuelto.py
+++ vuelto.py
@@ -1,30 +1,19 @@
-def vuelto (total ):
-    total= int(input("Ingrese el pago"))
-    
-    billete_10 = 0
-    billete_20 = 0 
-    billete_50 = 0 
-    billete_100 = 0 
-    billete_200 = 0
 
-    if total % 10 == 0 :
-        billete_10 = total / 10 
-    elif total % 20 == 0 :
-        billete_20 = total / 20
-    elif total % 50 == 0 :
-        billete_50 = total / 50
-    elif total % 100 == 0 :
-        billete_100 = total / 100
-    elif total % 200 == 0 :
-        billete_200 = total / 200 
+#Entrada
+def vuelto (monto_total):
+    monto_total= int(input("Ingrese el monto "))
 
-    print(billete_10)
-    print(billete_20)
-    print(billete_50)
-    print(billete_100)
-    print(billete_200) 
+    billete_10 = 0 
+    billete_20 = 0
+    billete_50 = 0
+    billete_100 = 0
+    billete_200 = 0 
+
+
 
     
+    
+
     
     
 
