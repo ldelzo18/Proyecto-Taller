a559916dd87fbd2daff293ed9638a01e164ea390
Jose Fabrizio Manco <josefabriziomanco@gmail.com>
2018-10-24 08:28:46 -0500

    Interfaz a medias xd

--- Interfaz.py
+++ Interfaz.py
@@ -7,7 +7,7 @@ sis.resizable(True,True)
 sis.geometry("800x500")
 sis.configure(bg = "beige")
 ttk.Button(sis, text='Finalizar Compra', command=quit).pack(side=BOTTOM)
-ttk.Button(sis, text='Añadir al carrito', command=quit).pack(side=LEFT)
+ttk.Button(sis, text='Añadir al carrito', command=quit).pack(side=BOTTOM)
 ttk.Label(sis, text='Producto: ').pack(side=LEFT)
 
 
