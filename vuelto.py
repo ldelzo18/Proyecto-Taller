a951a62501026a97ad3fb2cfe68cc193ac69ee7a
Jose <aldairxd11@hotmail.com>
2018-10-24 19:39:44 -0500

    version 2 - declarando contador a las monedas

--- vuelto.py
+++ vuelto.py
@@ -3,11 +3,18 @@
 def vuelto (monto_total):
     monto_total= int(input("Ingrese el monto "))
 
+    #Billetes
     billete_10 = 0 
     billete_20 = 0
     billete_50 = 0
     billete_100 = 0
-    billete_200 = 0 
+    billete_200 = 0
+
+    #Monedas
+    moneda_5 = 0
+    moneda_2 = 0 
+    moneda_1 = 0 
+
 
 
 
