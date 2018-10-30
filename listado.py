0519c5342f6bc4485e9c6444dd84d97d7fe42aac
jose luis <aldairxd11@hotmail.com>
2018-10-30 08:36:47 -0500

    listado v0

---
+++ listado.py
@@ -0,0 +1,20 @@
+elementos = []
+precio = []
+gaseosa = ["inka cola" , "Coca Cola" , "Fanta" , "4up" ]
+Galletas = ["Picaras" , "Oreo" , "Morochas " , "Margarita"]
+
+producto_usuario = input("Enter para mostrar una lista de productos y su precio")
+
+
+if gaseosa[0] == 'inka cola':
+        print ("El precio es de 2.40 soles") 
+        
+
+if gaseosa[1] == 'Coca Cola':
+        print("El precio es de 2.80 soles")
+
+if gaseosa[2] == 'Fanta':
+        print ("El precio es de 4.04 soles")
+
+if gaseosa[3] == '4up':
+        print ("El precio es de 5.50 soles")
