da1f396f34fa53a4684174099a766af782c2b4dc
Jose Fabrizio Manco <josefabriziomanco@gmail.com>
2018-10-31 08:49:09 -0500

    Implementacion de botones para abarrotes

--- funciones_botones.py
+++ funciones_botones.py
@@ -7,11 +7,17 @@ def botones_bebidas_es():
     beb.geometry("1000x400")
     beb.resizable(False,False)
     cantidad = Entry(beb).place (x=20, y=30)
-    inkaboton = Button(beb, text = "Inca Kola").place(x=50, y = 180)
-    cocaboton = Button(beb, text = "Coca Kola").place(x = 150, y = 180)
-    fantaboton = Button(beb, text = "Fanta").place(x= 300, y = 180) 
-   
-   
+    inkaboton = Button(beb, text =" Inca Kola").place(x=50, y = 150)
+    cocaboton = Button(beb, text =" Coca Kola").place(x = 150, y = 150)
+    fantaboton = Button(beb, text ="  Fanta  ").place(x= 300, y = 150) 
+    gateboton = Button(beb, text =" Gatorade ").place(x = 450, y= 150)
+    voltboton = Button(beb, text ="   Volt   ").place(x = 600, y= 150)
+    pepsiboton = Button(beb, text ="  Pepsi  ").place(x =50, y = 370)
+    freeboton = Button(beb, text ="  FreeTea ").place(x = 150,y =370)
+    sanboton = Button(beb, text="  San Luis  ").place(x=300, y=370)
+    cieloboton = Button(beb, text ="  Cielo  ").place(x = 450, y = 370)
+    sporboton = Button(beb, text="  Sporade  ").place(x= 600, y = 370)
+    
     beb.mainloop()
 
 
@@ -25,11 +31,54 @@ def botones_bebidas_en():
     beb.geometry("1000x400")
     beb.resizable(False,False)
     cantidad = Entry(beb).place (x=20, y=30)
-    inkaboton = Button(beb, text = "Inca Kola").place(x=50, y = 180)
-   cocaboton = Button(beb, text = "Coca Kola").place(x = 150, y = 180)
-    fantaboton = Button(beb, text = "Fanta").place(x= 300, y = 180)     
-    
+    inkaboton = Button(beb, text = "Inca Kola").place(x=50, y = 150)
+    cocaboton = Button(beb, text = "Coca Kola").place(x = 150, y = 150)
+    fantaboton = Button(beb, text = "Fanta").place(x= 300, y = 150) 
+    gateboton = Button(beb, text =" Gatorade ").place(x = 450, y= 150)
+    voltboton = Button(beb, text ="   Volt   ").place(x = 600, y= 150)
+    pepsiboton = Button(beb, text ="  Pepsi  ").place(x =50, y = 370)
+    freeboton = Button(beb, text ="  FreeTea ").place(x = 150,y =370)
+    sanboton = Button(beb, text="  San Luis  ").place(x=300, y=370)
+    cieloboton = Button(beb, text ="  Cielo  ").place(x = 450, y = 370)
+    sporboton = Button(beb, text="  Sporade  ").place(x= 600, y = 370)
     
     beb.mainloop()
-    
 
+def botones_abarrotes_es():
+    aba = Tk()
+    aba.title("Catalogo de Abarrotes")
+    aba.geometry("1000x400")
+    aba.resizable(False,False)
+    cantidad = Entry(aba).place(x = 20, y = 30)
+    picaboton = Button(beb, text = "Picaras").place(x=50, y = 150)
+    laysboton = Button(beb, text = " Lays ").place(x = 150, y = 150)
+    dorboton = Button(beb, text = "Doritos").place(x= 300, y = 150) 
+    sodboton = Button(beb, text =" Soda Field ").place(x = 450, y= 150)
+    iberboton = Button(beb, text ="   Iberica  ").place(x = 600, y= 150)
+    subboton = Button(beb, text ="  Sublime  ").place(x =50, y = 370)
+    piqboton = Button(beb, text =" Piqueo Snack ").place(x = 150,y =370)
+    doñaboton = Button(beb, text="  Doña Pepa  ").place(x=300, y=370)
+    cereaboton = Button(beb, text ="  Cereal Bar  ").place(x = 450, y = 370)
+    chokisboton = Button(beb, text="  Chokis  ").place(x= 600, y = 370)
+
+    aba.mainloop()
+
+def botones_abarrotes_en():
+    aba = Tk()
+    aba.title("Catalogo de Abarrotes")
+    aba.geometry("1000x400")
+    aba.resizable(False,False)
+    cantidad = Entry(aba).place(x = 20, y = 30)
+    picaboton = Button(beb, text = "Picaras").place(x=50, y = 150)
+    laysboton = Button(beb, text = " Lays ").place(x = 150, y = 150)
+    dorboton = Button(beb, text = "Doritos").place(x= 300, y = 150) 
+    sodboton = Button(beb, text =" Soda Field ").place(x = 450, y= 150)
+    iberboton = Button(beb, text ="   Iberica  ").place(x = 600, y= 150)
+    subboton = Button(beb, text ="  Sublime  ").place(x =50, y = 370)
+    piqboton = Button(beb, text =" Piqueo Snack ").place(x = 150,y =370)
+    doñaboton = Button(beb, text="  Doña Pepa  ").place(x=300, y=370)
+    cereaboton = Button(beb, text ="  Cereal Bar  ").place(x = 450, y = 370)
+    chokisboton = Button(beb, text="  Chokis  ").place(x= 600, y = 370)
+
+
+    aba.mainloop()    
