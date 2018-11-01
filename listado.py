e882ffab81c7d663dd24510179f7d49cf96c9fbe
Jose <aldairxd11@hotmail.com>
2018-10-31 19:35:27 -0500

    version final poner float al  monto

--- listado.py
+++ listado.py
@@ -1,20 +1,32 @@
 
 contador_incacola = 0
 contador_cocacola = 0
+contador_fanta = 0
+contador_pepsi = 0 
+
 contador_volt = 0
 contador_gaterode = 0
 contador_sporade = 0
-contador_fanta = 0
+contador_freetea = 0
 
-nombre_producto =["Inca Kola" , "Volt" , "Coca Cola" , "Fanta" , "gaterode"]
-precio_correspondiente = [2.45 , 3.00 , 2.45 , 3.00 , 4.00]
+contador_san_mateo = 0
+contador_cielo = 0
 
+#Lista Bebidas energetica
+bebidas_energetica =["Gaterode 500 ml " ,"Volt 330 ml" , "FreeTea" , "Sporade"]
+precio_bebidas_energetica = [2.50 , 3.00 , 3.50 , 4.00]
+#Lista Gaseosa
+Gaseosa_producto =["Coca Cola 450 ml " , "Inca Cola 450 ml" , "Fanta 400 ml" , "Pepsi 500 ml"]
+Precio_Gaseosa = [2.45 , 2.45 , 3.00 , 1.80]
+#Lista agua
+agua_producto =["San Mateo 450 ml" , "Cielo 620 ml"]
+precio_agua = [3.50 , 2.00]
 
 while True :
         seleccion =  input ("Seleccione \n1=Bebidas \n2=Galletas\n" )
         if (seleccion == "1"):
                 while True :
-                        seleccion_bebidas = input("Seleccione \n 1=Gaseosa \n 2=Bedidas Energeticas \n 3=Vino \n")
+                        seleccion_bebidas = input("Seleccione \n 1=Gaseosa \n 2=Bedidas Energeticas \n 3=Bebidas(agua) \n")
 
                         if (seleccion_bebidas == "1") :
                                 while True :
@@ -30,6 +42,7 @@ while True :
                                                 for i in range (cantidad_cocacola) :
                                                         contador_cocacola += 1     
                                                 break
+
                                         if (seleccion_gaseosa == "3"):
                                                 cantidad_fanta = int(input("seleccione cuantas deseas llevar \n"))
                                                 for i in range (cantidad_fanta) :
@@ -39,38 +52,64 @@ while True :
 
                         if (seleccion_bebidas == "2") :
                                 while True :
-                                        seleccion_bebidas_energetica = input("Seleccione las marcas disponible  \n 1=Volt 330ml \n 2=Gaterode \n 3=Sporade \n")
+                                        seleccion_bebidas_energetica = input("Seleccione las marcas disponible  \n 1=Volt 330ml \n 2=Gaterode \n 3=Sporade \n 4=FreeTea \n")
                                         if(seleccion_bebidas_energetica == "1"):
                                                 cantidad_volt = int(input("Seleccione cuantas deseas llevar \n"))
                                                 for i in range (cantidad_volt):
                                                         contador_volt += 1
+                                                break
 
-                                                        break
                                         if(seleccion_bebidas_energetica == "2"):
                                                 cantidad_gaterode = int(input("Seleccione cuantas deseas llevar \n"))
                                                 for i in range (cantidad_gaterode):
                                                         contador_gaterode += 1
-                                                        break
+                                                break
+
                                         if(seleccion_bebidas_energetica == "3"):
                                                 cantidad_sporade = int(input("Seleccione cuantas deseas llevar \n"))
                                                 for i in range (cantidad_sporade):
                                                         contador_sporade += 1
-                                                        break
-                                                       
-                        
+                                                break
+
+                                        if(seleccion_bebidas_energetica == "4"):
+                                                cantidad_freetea = int(input("Seleccione cuantas deseas llevar \n"))
+                                                for i in range (cantidad_freetea):
+                                                        contador_freetea += 1
+                                                break
+                        if (seleccion_bebidas == "3"):
+                                while True :
+                                        seleccion_agua = input("Seleccione las marcas disponible  \n 1=San Mateo \n 2=Cielo \n")
+                                        if(seleccion_agua == "1"):
+                                                cantidad_san_mateo = int(input("Seleccione cuantas deseas llevar \n"))
+                                                for i in range (cantidad_san_mateo):
+                                                        contador_san_mateo += 1
+                                                break
+                                        if(seleccion_agua == "2"):
+                                                cantidad_cielo = int(input("Seleccione cuantas deseas llevar \n"))
+                                                for i in range ( cantidad_cielo) :
+                                                        contador_cielo +=1
                                         break
                         break
 
         if (contador_gaterode >= 1 ):
-                print ("Cantidad :" , contador_gaterode ,"Nombre" ,nombre_producto[4] ,"Precio por unidad",precio_correspondiente[4])
+                print ("Cantidad :" , contador_gaterode ,"Nombre" ,bebidas_energetica[0] ,"Precio por unidad",precio_bebidas_energetica[0])
         if (contador_cocacola >= 1) :
-                print ("Cantidad :" , contador_cocacola ,"Nombre" ,nombre_producto[2] ,"Precio por unidad",precio_correspondiente[0] )
+                print ("Cantidad :" , contador_cocacola ,"Nombre" ,Gaseosa_producto[0] ,"Precio por unidad",Precio_Gaseosa[0] )
         if (contador_incacola >= 1) :
-                print ("Cantidad :" , contador_incacola ,"Nombre" ,nombre_producto[0] ,"Precio por unidad",precio_correspondiente[0] )
+                print ("Cantidad :" , contador_incacola ,"Nombre" ,Gaseosa_producto[1] ,"Precio por unidad",Precio_Gaseosa[1] )
         if (contador_volt >= 1) :
-                print ("Cantidad :" , contador_volt , "Nombre" , nombre_producto[1] ,"Precio por unidad" , precio_correspondiente[1])
+                print ("Cantidad :" , contador_volt , "Nombre" , bebidas_energetica[1] ,"Precio por unidad" , precio_bebidas_energetica[1])
         if (contador_fanta >= 1 ):
-                print("Cantidad :" , contador_fanta , "Nombre" , nombre_producto[3] ,"Precio por unidad" , precio_correspondiente[3])
+                print("Cantidad :" , contador_fanta , "Nombre" , Gaseosa_producto[2] ,"Precio por unidad" , Precio_Gaseosa[2])
+        if (contador_freetea >= 1 ):
+                print("Cantidad :" , contador_freetea , "Nombre" , bebidas_energetica[2] ,"Precio por unidad" , precio_bebidas_energetica[2])
+        if (contador_sporade >= 1 ):
+                print("Cantidad :" , contador_sporade , "Nombre" , bebidas_energetica[3] ,"Precio por unidad" , precio_bebidas_energetica[3])
+        if (contador_san_mateo >= 1 ):
+                print("Cantidad :" , contador_san_mateo , "Nombre" , agua_producto[0] ,"Precio por unidad" , precio_agua[0])
+        if (contador_cielo >= 1 ):
+                print("Cantidad :" , contador_cielo , "Nombre" , agua_producto[1] ,"Precio por unidad" , precio_agua[1])
+        
         
 
 
@@ -79,8 +118,3 @@ while True :
                 print ("gracias ;)") 
                 break  
 
-##print("cantidad de sporade " , contador_sporade)
-       ### print ("cantidad de gaterode " , contador_gaterode)
-       # print ("cantidad de cocacola " ,contador_cocacola)
-       # print ("cantidad de inca cola " 
-          
--- vuelto.py
+++ vuelto.py
@@ -2,7 +2,7 @@
 #Entrada
 def vuelto ():
     while True :
-        monto_total= int(input("Ingrese el monto "))
+        monto_total= float(input("Ingrese el monto "))
         # El monto total deve ser positivo
         if monto_total >= 0 :
             break
@@ -18,6 +18,9 @@ def vuelto ():
     moneda_5 = 0
     moneda_2 = 0 
     moneda_1 = 0 
+    monedad_50centavos = 0 
+    moneda_20centavos = 0
+    moneda_10centavos = 0
 
     #Billete de 200 soles
     billete_200 = (int(monto_total/200))
@@ -49,6 +52,16 @@ def vuelto ():
     #Moneda de 1 sol
     moneda_1 = (int(monto_total / 1 ))
     monto_total = monto_total - 1 * moneda_1
+    #Moneda de 50 centavos = 0.50 soles
+    moneda_50centavos = (int(monto_total / 0.50 ))
+    monto_total = monto_total - 0.50 * moneda_50centavos
+    #Moneda de 20 centavos = 0.20 soles
+    moneda_20centavos = (int(monto_total / 0.20 ))
+    monto_total = monto_total - 0.20 * moneda_20centavos
+    #Moneda de 10 centavos = 0.10 soles
+    moneda_10centavos = (int(monto_total / 0.10 ))
+    monto_total = monto_total - 0.10 * moneda_10centavos
+
     print("Cantidad de S/ 200" , billete_200)
     print("Cantidad de S/ 100" , billete_100)
     print("Cantidad de S/ 50 " , billete_50)
@@ -57,6 +70,9 @@ def vuelto ():
     print("Cantidad de S/ 5 "  , moneda_5)
     print("Cantidad de S/ 2 "  , moneda_2)
     print("Cantidad de S/ 1 "  , moneda_1)
+    print("Cantidad de 50 centavos" , moneda_50centavos)
+    print("Cantidad de 20 centavos" , moneda_20centavos)
+    print("Cantidad de 20 centavos" , moneda_10centavos)
 
 vuelto()
 print("Gracias por la compra")
