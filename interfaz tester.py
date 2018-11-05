5e9827ab5643835621d336c9b6c5aa92dcdf8b45
Luis Delzo Espinoza <ldelzo.e@gmail.com>
2018-11-05 16:52:05 -0500

    Moviendo las imagenes a carpeta Img

--- interfaz tester.py
+++ interfaz tester.py
@@ -1,5 +1,6 @@
 #prueba
 from tkinter import *
+
 from interfaces import *
 
 sis = Tk()
@@ -8,11 +9,11 @@ sis.geometry("300x300")#300x350
 sis.configure(bg = "white")
 sis.title("Sistema de ventas")
 
-miLogo=PhotoImage(file="Logo2.gif")
+miLogo=PhotoImage(file="Img/Logo2.gif")
 
 
 ttk.Label(sis,image=miLogo).place(x=90,y=40)
-ttk.Label(sis,text='       Bienvenido a OXXO!\nEliga su idioma de preferencia ',font=("Comic Sans MS",12)).place(x=40,y=95)
+ttk.Label(sis,text='       Bienvenido a OXXO!\nEliga su idioma de preferencia',font=("Comic Sans MS",12)).place(x=40,y=95)
 ttk.Button(sis, text='English',command=gIEnglish).place(x=100,y=160)
 ttk.Button(sis,text='Español',command=gISpanish).place(x=100,y=210)
 
