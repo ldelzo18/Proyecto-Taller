e9a483bbea0385694be67826e4cbf1262ac4db98
Luis Delzo Espinoza <ldelzo.e@gmail.com>
2018-11-05 23:15:11 -0500

    Grid implementado en bebidas y abarrotes

--- interfaces.py
+++ interfaces.py
@@ -45,22 +45,22 @@ def gIEnglish():
     miLabel = ttk.Label(root,text = "CATEGORIES", font = ("TIMES NEW ROMAN", 16))
     miLabel.grid(row=0,column=0,padx=20)   
 
-    miBoton1 = ttk.Button(root, text = "Drinks", width= 18,command =botones_bebidas_es)
+    miBoton1 = ttk.Button(root, text = "Drinks", width= 18,command =botones_bebidas_en)
     miBoton1.grid(row=1,column=0,padx=20,pady=5)
 
-    miBoton2 = ttk.Button(root, text = "Groceries",width=18,command =botones_abarrotes_es)
+    miBoton2 = ttk.Button(root, text = "Groceries",width=18,command =botones_abarrotes_en)
     miBoton2.grid(row=2,column=0,padx=20)
 
-    miBoton3 = ttk.Button(root, text = "Frozen Food",width=18,command =botones_congelados_es)
+    miBoton3 = ttk.Button(root, text = "Frozen Food",width=18,command =botones_congelados_en)
     miBoton3.grid(row=3,column=0,padx=20)
 
-    miBoton4 = ttk.Button(root, text = "Ready to eat!",width=18,command =botones_comida_preparada_es)
+    miBoton4 = ttk.Button(root, text = "Ready to eat!",width=18,command =botones_comida_preparada_en)
     miBoton4.grid(row=4,column=0,padx=20)
 
-    miBoton5 = ttk.Button(root, text = "Personal Care",width=18,command =botones_cuidado_personal_es)
+    miBoton5 = ttk.Button(root, text = "Personal Care",width=18,command =botones_cuidado_personal_en)
     miBoton5.grid(row=5,column=0,padx=20)
 
-    miBoton6 = ttk.Button(root, text = "Pagar",width=15,command = quit)
+    miBoton6 = ttk.Button(root, text = "Pay",width=15,command = quit)
     miBoton6.grid(row=6,column=0,padx=20)
 
     txt = Text(root,width=50,heigh=35,wrap=WORD)
