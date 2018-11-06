f3f4e892820f3b916dd07a7d825e446a13f407d6
Jose <aldairxd11@hotmail.com>
2018-11-06 14:58:28 -0500

    agregando funcion a los productos de cuidado personal(corregido)

--- funciones_productos.py
+++ funciones_productos.py
@@ -37,53 +37,47 @@ def addSanLuis(lista_Producto,lista_Precios,cantidad):
 def addSporade(lista_Producto,lista_Precios,cantidad):
         for x in range(0,cantidad):
                 lista_Producto.append("Sporade 500ml")
-                lista_Precios.append(2.00)                             
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                lista_Precios.append(2.00) 
+def addjabon(lista_Producto,lista_Precios,cantidad):
+        for x in range(0,cantidad):
+                lista_Producto.append("Jabon Dove")
+                lista_Precios.append(3.00)
+def addshampoo(lista_Producto,lista_Precios,cantidad):
+        for x in range(0,cantidad):
+                lista_Producto.append("H&S Shampoo Old Spice")
+                lista_Precios.append(14.00)
+def addpasta(lista_Producto,lista_Precios,cantidad):
+        for x in range(0,cantidad):
+                lista_Producto.append("Colgate Toothpaste")
+                lista_Precios.append(7.00)
+def addcepillo(lista_Producto,lista_Precios,cantidad):
+        for x in range(0,cantidad):
+                lista_Producto.append("Colgate Toothbrush")
+                lista_Precios.append(4.00)
+def addgillet(lista_Producto,lista_Precios,cantidad):
+        for x in range(0,cantidad):
+                lista_Producto.append("Hoja de afeitar Gillet")
+                lista_Precios.append(20.00)
+def addtoallas(lista_Producto,lista_Precios,cantidad):
+        for x in range(0,cantidad):
+                lista_Producto.append("Toallas femeninas LadySoft")
+                lista_Precios.append(3.00)
+ def addlisterine(lista_Producto,lista_Precios,cantidad):
+        for x in range(0,cantidad):
+                lista_Producto.append("Listerine 500 ML")
+                lista_Precios.append(5.50)
+ def adddento(lista_Producto,lista_Precios,cantidad):
+        for x in range(0,cantidad):
+                lista_Producto.append("Pasta Dento")
+                lista_Precios.append(8.50)
+ def addcrema(lista_Producto,lista_Precios,cantidad):
+        for x in range(0,cantidad):
+                lista_Producto.append("Crema de Afeitar Gillete")
+                lista_Precios.append(12.00)
+def addheadshoulder(lista_Producto,lista_Precios,cantidad):
+        for x in range(0,cantidad):
+                lista_Producto.append("Head & Shoulders 375 ML")
+                lista_Precios.append(10.00)   
 def addPicaras(lista_Producto,lista_Precios,cantidad):
         for x in range(0,cantidad):
                 lista_Producto.append("Picaras")
