9fa2baf8e689811cbf692e1c8b7326d6587e74d8
Luis Delzo Espinoza <ldelzo.e@gmail.com>
2018-11-05 20:19:50 -0500

    se implemento "Text" en ambos idiomas

--- interfaces.py
+++ interfaces.py
@@ -4,29 +4,34 @@ from funciones_botones import *
 def gISpanish():
     root = Tk()
     root.title("Sistema de Ventas")
-    root.geometry("800x400")
+    root.geometry("600x600")
     root.resizable(False,False)
 
-    miLabel = ttk.Label(root, text = "CATEGORIAS", font = ("TIMES NEW ROMAN", 19))
-    miLabel.grid(row=0,column=1,pady=5,padx=10)
+    miLogo=PhotoImage(file="Img/Logo2.gif")
+
+    miLabel = ttk.Label(root,text = "CATEGORIAS", font = ("TIMES NEW ROMAN", 16))
+    miLabel.grid(row=0,column=0,padx=20)   
 
     miBoton1 = ttk.Button(root, text = "Bebidas", width= 18,command =botones_bebidas_es)
-    miBoton1.grid(row=2,column=1,pady=15,padx=10)
+    miBoton1.grid(row=1,column=0,padx=20,pady=5)
 
     miBoton2 = ttk.Button(root, text = "Abarrotes",width=18,command =botones_abarrotes_es)
-    miBoton2.grid(row=4,column=1,pady=15,padx=10)
+    miBoton2.grid(row=2,column=0,padx=20)
 
     miBoton3 = ttk.Button(root, text = "Congelados",width=18,command =botones_congelados_es)
-    miBoton3.grid(row=6,column=1,pady=15,padx=10)
+    miBoton3.grid(row=3,column=0,padx=20)
 
     miBoton4 = ttk.Button(root, text = "Comida Preparada",width=18,command =botones_comida_preparada_es)
-    miBoton4.grid(row=8,column=1,pady=15,padx=10)
+    miBoton4.grid(row=4,column=0,padx=20)
 
     miBoton5 = ttk.Button(root, text = "Cuidado Personal",width=18,command =botones_cuidado_personal_es)
-    miBoton5.grid(row=10,column=1,pady=15,padx=10)
+    miBoton5.grid(row=5,column=0,padx=20)
 
     miBoton6 = ttk.Button(root, text = "Pagar",width=15,command = quit)
-    miBoton6.grid(row=12,column=5,pady=15,padx=10)
+    miBoton6.grid(row=6,column=0,padx=20)
+
+    txt = Text(root,width=50,heigh=35,wrap=WORD)
+    txt.grid(row=0,column=1,rowspan=7,sticky=E)
 
     #entry_salida = Entry(root,state=DISABLED,width=80).place(x=280,y=50)
 
@@ -35,28 +40,30 @@ def gISpanish():
 def gIEnglish():
     root = Tk()
     root.title("Sales System")
-    root.geometry("800x400")
+    root.geometry("600x600")
 
-    miLabel = ttk.Label(root, text = "CATEGORIES", font = ("TIMES NEW ROMAN", 19))
-    miLabel.grid(row=0,column=1,pady=5,padx=10)
+    miLabel = ttk.Label(root,text = "CATEGORIES", font = ("TIMES NEW ROMAN", 16))
+    miLabel.grid(row=0,column=0,padx=20)   
 
     miBoton1 = ttk.Button(root, text = "Drinks", width= 18,command =botones_bebidas_es)
-    miBoton1.grid(row=2,column=1,pady=15,padx=10)
+    miBoton1.grid(row=1,column=0,padx=20,pady=5)
 
     miBoton2 = ttk.Button(root, text = "Groceries",width=18,command =botones_abarrotes_es)
-    miBoton2.grid(row=4,column=1,pady=15,padx=10)
+    miBoton2.grid(row=2,column=0,padx=20)
 
-    miBoton3 = ttk.Button(root, text = "Frozen Foods",width=18,command =botones_congelados_es)
-    miBoton3.grid(row=6,column=1,pady=15,padx=10)
+    miBoton3 = ttk.Button(root, text = "Frozen Food",width=18,command =botones_congelados_es)
+    miBoton3.grid(row=3,column=0,padx=20)
 
-    miBoton4 = ttk.Button(root, text = "Ready to Take!",width=18,command =botones_comida_preparada_es)
-    miBoton4.grid(row=8,column=1,pady=15,padx=10)
+    miBoton4 = ttk.Button(root, text = "Ready to eat!",width=18,command =botones_comida_preparada_es)
+    miBoton4.grid(row=4,column=0,padx=20)
 
-    miBoton5 = ttk.Button(root, text = "Personal care",width=18,command =quit)
-    miBoton5.grid(row=10,column=1,pady=15,padx=10)
+    miBoton5 = ttk.Button(root, text = "Personal Care",width=18,command =botones_cuidado_personal_es)
+    miBoton5.grid(row=5,column=0,padx=20)
 
-    miBoton6 = ttk.Button(root, text = "Pay",width=15,command = quit)
-    miBoton6.grid(row=12,column=5,pady=15,padx=10)
+    miBoton6 = ttk.Button(root, text = "Pagar",width=15,command = quit)
+    miBoton6.grid(row=6,column=0,padx=20)
 
+    txt = Text(root,width=50,heigh=35,wrap=WORD)
+    txt.grid(row=0,column=1,rowspan=7,sticky=E)
 
     root.mainloop() 
