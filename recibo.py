9cfceacf32bf0719588068aa9feda62599ece0ed
Jose Luis <aldairxd11@hotmail.com>
2018-11-07 08:28:12 -0500

    recibo agregado

---
+++ recibo.py
@@ -0,0 +1,34 @@
+def recibo ():
+    lista_producto =["d" , 4]
+    lista_precios = ["ff" , 4]
+    lista_temp =[]
+    
+    
+    for lista in lista_producto :
+        lista_temp.append(lista_producto)
+    for lista in lista_precios :
+        lista_temp.append(lista_precios)
+
+
+    print (lista_temp)
+        
+
+        
+        
+
+    
+        
+    
+
+recibo()
+
+
+
+
+
+        
+    
+
+
+        
+
